7aad990316d5b9e6548f09d57b53e93be0066d61
Victor <48195230+JackieMaHatesDogEyes@users.noreply.github.com>
2023-07-20 01:09:58 -0400

    Revamp Code and add README.

---
+++ README.md
@@ -0,0 +1,5 @@
+# SatDL - A Python Satellite Imagery Downloader.
+
+Can currently get raster data from ESRI (ArcGIS), Google Satellite, and OpenStreetMap.  
+
+It can also convert Web Mercator to Equirectangular projection (requires GDAL installed).
--- SatDL/__init__.py
+++ SatDL/__init__.py
@@ -1,76 +1,144 @@
 import requests
-import os
+
 from io import BytesIO
+
 from PIL import Image
 
-class SatDownloader:
-    def __init__(self):
-        self.URL = "https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{x}/{y}"
+from tempfile import NamedTemporaryFile
 
-    def GetTile(self, name: str, zoom:int, x:int, y:int, fp=None):
+import os
 
-        if x >= (2 ** zoom) or y >= (2 ** zoom):
-            raise Exception("Invalid Zoom.")
+class Projection:
+    EQUIRECTANGULAR = 4326
+    WEBMERCATOR = 3857
+
+    SQUARE = 3857
+    RECTANGULAR = 4326
 
 
-        url = self.URL.replace("{z}", str(zoom)).replace("{x}", str(x)).replace("{y}", str(y))
-        response = requests.get(url)
+def ArcGIS (z : int, x : int, y : int):
+    return f"https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
 
+def Google (z : int, x : int, y : int):
+    return f"https://mt.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+
+def OSM(z : int, x : int, y : int):
+    return f"https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+def OpenStreetMap(z : int, x : int, y : int):
+    return OSM(z, x , y)
+
+class SatImage:
+    def __init__(self, image: Image):
+        self.Image = image
+
+    def __str__(self):
+        return f"SatImage Instance ({repr(self)})"
+
+    def Export(self, name: str, extension='jpg', projection: int = 3857,  folder="."):
+        # 3857 = Web Mercator (default)
+        # 4326 = Equirectangular
+        if projection == 4326:
+            os.chdir(folder)
+            img = NamedTemporaryFile("wb", suffix=".tif", dir=".", delete=False)
+            self.Image.save(img, format='TIFF')
+            
+            img.flush()
+            
+            os.system(f"gdal_translate -a_srs EPSG:3857 -a_ullr -20037508 20037508 20037508 -20037508 {img.name} ./output.tif")
+            os.system(f"gdalwarp -s_srs EPSG:3857 -t_srs EPSG:4326 output.tif intermediate.tif")
+            img.close()
+            os.remove(img.name)
+            os.remove("output.tif")
+
+            if "tif" not in extension or "tiff" not in extension:
+                temp = Image.open("intermediate.tif")
+                temp.save(f"{name}.{extension}")
+                os.remove("intermediate.tif")
+            else:
+                os.replace("intermediate.tif", f"{name}.{extension}")
+            
+
+            os.chdir("../")
+            
+
+        elif projection == 3857:
+            self.Image.save(f"{name}.{extension}")
+        
+        else:
+            raise Exception(f"Invalid Projection (or not supported): {projection}")
+
+    
+
+    def Download(zoom : int, x : int, y : int, source = ArcGIS):
+
+        
+        try:
+            url = source(zoom, x, y)
+
+
+        except AttributeError:
+            if isinstance(source, str):
+                url = source
+            else:
+                raise Exception(f"Invalid source: '{source}'")
+
+        headers = {
+            'User-Agent': 'M'
+        } # Bypass User Agent Limits
+        response = requests.get(url, headers=headers)
         if response.status_code != 200:
             raise Exception(f"Got error code: {response.status_code}")
 
-        image = response.content
-        print(url)
-        if not fp:
-            with open(f"{name}.jpg", "wb") as file:
-                file.write(image)
-        else:
-            fp.write(image)
-            return fp
+
+        img = BytesIO()
+        img.write(response.content)
+
+        image = Image.open(img)
+
+        return SatImage(image)
+
+   
+
+
+def GetWorld(zoom:int, source = ArcGIS):
+    max_range = 2 ** zoom
+
+    img_arr = [
+        [] for _ in range(max_range)
+    ]
+
+    image_res = max_range * 256
+
+
+
+
+
+
+    counter = 0
+
+    for x in range(0, max_range):
+        for y in range(0, max_range):
+            img = SatImage.Download(zoom, x, y, source=source)
+            img_arr[y].append(img.Image)
+            print(f"Downloading Tile: {x}:{y}")
+        counter += 1
+
+    image = Image.new('RGB', (256 * len(img_arr), 256 * len(img_arr)))
+
+    w_counter = 0
+    h_counter = 0
+
+    for h in img_arr:
+        for w in h:
+            image.paste(im=w, box=(w_counter, h_counter))
+            w_counter += 256
+        w_counter = 0
+        h_counter += 256
+
+    return SatImage(image)
+
+
+
+
 
         
-
-    def GetWorldAtZoom(self, zoom: int, folder:str = "out", filename="world", extension="jpg"):
-        image_array = [
-            [] for _ in range(2 ** zoom)
-        ]
-
-        max_zoom = 2 ** zoom
-
-        final_image_res = (2 ** zoom) * 256 # resolution (256px x 256px)
-        
-
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-        os.chdir(folder)
-
-        
-        counter = 0
-
-        for x in range(0, max_zoom): # x
-
-            for y in range(0, max_zoom):
-                img = BytesIO()
-                img = self.GetTile(f'worldx{y}y{x}', zoom, x, y, img)
-                img = Image.open(img) 
-                image_array[x].append(img)
-                print(y)
-            counter += 1
-
-        image = Image.new('RGB', (256 * len(image_array), 256 * len(image_array)))
-
-        width_counter = 0
-        height_counter = 0
-        for h in image_array:
-            for w in h:
-                image.paste(im=w, box=(width_counter, height_counter))
-                width_counter += 256
-            width_counter = 0
-            height_counter += 256
-
-        image.save(f"{filename}.{extension}")
-        
-
-        
-
-
